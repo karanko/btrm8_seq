d6e2ba63dbc6897a9f34648232f0038779cab327
karanko <karanko@cs30>
2020-05-05 10:13:48 +0100

    add sub pages split colouring tidy up add standard seq x4

--- apcmini.py
+++ apcmini.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python
 import sys
 import time
 import socket
@@ -9,6 +10,7 @@ class Page:
 	def __init__(self, name):
 		self.name=  name
 		self.grid = [False] * 64
+		self.h_splits = 1
 		self.outputarray = []
 		self.__press_callbacks = []
 		self.__data_callbacks = []
@@ -40,9 +42,14 @@ class PageManager:
 			self.udpsocket.sendto( message + ";\n", ("127.0.0.1", port))
 		except socket.error as e:
 			print e
-	def addpage(self,name):
+	def addpage(self,name,datacallback=None,presscallback=None,h_splits=1):
 		workingpage = Page(name)
 		self.pages.append(workingpage)
+		workingpage.h_splits = h_splits
+		if datacallback:
+			workingpage.adddatacallback(datacallback)
+		if presscallback:
+			workingpage.addpresscallback(presscallback)
 		return workingpage
 	def currentpage(self):
 		return self.pages[self.currentpage_index]
@@ -58,20 +65,27 @@ class PageManager:
 		return res
 	def senddatatopd(self):
 		page = self.currentpage()
-		ugly_data = str( numpy.matrix(page.outputarray)).replace('[','').replace(']','').split('\n')
-		for i in range(len(ugly_data)):
-			self.sendfudi("apcmini " + page.name + " " + str(i) + " data " + ugly_data[i] ,3000)
+		try:
+			ugly_data = str( numpy.matrix(page.outputarray)).replace('[','').replace(']','').split('\n')
+			for i in range(len(ugly_data)):
+				self.sendfudi("apcmini " + page.name + " " + str(i) + " data " + ugly_data[i] ,3000)
+		except Exception, e:
+			print("An exception occurred: %s" % (e))
 	def draw(self):
-		for i in range(len(self.currentpage().grid)):
-			if self.currentpage().grid[i]:
-				self.setbuttonstate(i,(((self.currentpage_index  % 3) + 1) * 2)-1 )
-			else:
-				self.setbuttonstate(i,0)
-		for x in range(68, 72):
-			self.setbuttonstate( x , 0 )
-		self.setbuttonstate(68+self.currentpage_index, 1 )
-		#not sure if this is the right place
-		self.senddatatopd()
+		try:
+			for i in range(len(self.currentpage().grid)):
+				if self.currentpage().grid[i]:
+					sub_page_offset = i / (64 / self.currentpage().h_splits ) 
+					self.setbuttonstate(i,((((self.currentpage_index + sub_page_offset) % 3) + 1) * 2)-1 )
+				else:
+					self.setbuttonstate(i,0)
+			for x in range(68, 72):
+				self.setbuttonstate( x , 0 )
+			self.setbuttonstate(68+self.currentpage_index, 1 )
+			#not sure if this is the right place
+			self.senddatatopd()
+		except Exception, e:
+			print("An exception occurred: %s" % (e))
 	def setbuttonstate(self,pad,state):
 			#we only need to send note ons to avoid traffic
 			midiout.send_message([144 & 0xF0 | 0 & 0xF, pad & 0x7F , state& 0x7F])
@@ -79,25 +93,69 @@ class PageManager:
 			self.sendfudi("apcmini stopallclips " + str(int(isdown == True)) + " " +  str(int(self.shiftdown == True))   )
 	def midiin_callback(self,event, data=None):
 		message, deltatime = event
+		try:
+			if message[0] in [128,144] and message[1] == 98:
+					self.shiftdown = message[0] == 144
+			elif message[0] in [128,144]:
+				if 0 <= message[1] <= 63 and message[0] == 128:
+					pm.currentpage().pressgrid(message[1])
+					self.draw()
+				elif 68 <= message[1] <= 71 and message[0] == 144:
+					##page selcection
+					self.setpage(message[1] - 68)
+				elif message[1] == 89:
+					self.stopallclips(message[0] == 144)
+					self.setbuttonstate( message[1] , int( message[0] == 144) )
+				elif 64 <= message[1] <= 67 and message[0] == 128:
+					dir = message[1]-64
+				else:
+					print("note %r" % ( message))
+			elif message[0] == 176:             	
+				self.sendfudi("apcmini fader " + str(message[1] - 48) + " " + str(message[2])  , 3000)
+			else:
+				print("PageManager: Unhandled msg: %r" % ( message ))
+		except Exception, e:
+			print("An exception occurred: %s" % (e))
+def metro_rep_press_cb(page,pad):
+	for p in range(64):
+		col = p % 8
+		row = (p / 8) 
+		page.grid[p] = row <= page.outputarray[col]
+def metro_rep_data_cb(page,pad):
+	if len(page.outputarray) ==0:
+		page.outputarray = [0] * 8
+	col = pad % 8
+	row = (pad / 8) 
+	page.outputarray[col] = row
+def metro_gate_press_cb(page,pad):
+	for p in range(64):
+		col = p % 8
+		row = (p / 8) % 4
+		sec =  int(p < 32) 
+		page.grid[p] = row <= page.outputarray[sec][col]
+def metro_gate_data_cb(page,pad):
+	if len(page.outputarray) ==0:
+		page.outputarray = [[]] * 2
+		for x in range(len(page.outputarray)):
+			page.outputarray[x] = [0] * 8
+	col = pad % 8
+	row = (pad / 8) % 4
+	sec =  int(pad < 32)
+	page.outputarray[sec][col] = row
+def std_press_cb(page,pad):
+	for p in range(64):
+		col = p % 16
+		sec =  p / 16
+		page.grid[p] = page.outputarray[sec][col]
+def std_data_cb(page,pad):
+	if len(page.outputarray) ==0:
+		page.outputarray = [[]] * 4
+		for x in range(len(page.outputarray)):
+			page.outputarray[x] = [0] * 16
+	col = pad % 16
+	sec = pad / 16 
+	page.outputarray[sec][col] = int(not bool(page.outputarray[sec][col]))
 
-		if message[0] in [128,144] and message[1] == 98:
-				self.shiftdown = message[0] == 144
-		elif message[0] in [128,144]:
-			if 0 <= message[1] <= 64 and message[0] == 128:
-				pm.currentpage().pressgrid(message[1])
-				self.draw()
-			elif 68 <= message[1] <= 71 and message[0] == 144:
-				##page selcection
-				self.setpage(message[1] - 68)
-			elif message[1] == 89:
-				self.stopallclips(message[0] == 144)
-				self.setbuttonstate( message[1] , int( message[0] == 144) )
-			#else:
-				#print("note %r" % ( message))
-		elif message[0] == 176:             	
-			self.sendfudi("apcmini fader " + str(message[1] - 48) + " " + str(message[2])  , 3000)
-		else:
-			print("PageManager: Unhandled msg: %r" % ( message ))
 
 ## setup out
 midiout = rtmidi.MidiOut()
@@ -121,68 +179,13 @@ for i, j in enumerate(midiin.get_ports()):
 			break
 		except (EOFError, KeyboardInterrupt):
 			sys.exit()
+					
+pm = PageManager()		
+pm.addpage("metropolis_repeats",metro_rep_data_cb,metro_rep_press_cb)
+pm.addpage("metropolis_gate_dual",metro_gate_data_cb,metro_gate_press_cb,2)
+pm.addpage("metropolis_pitch",metro_rep_data_cb,metro_rep_press_cb)
+pm.addpage("std_seq_x4",std_data_cb,std_press_cb,4)
 
-##build metropolis class to tidy things up??
-
-## let the bodge begin
-pm = PageManager()
-##rpage 0
-mr = pm.addpage("metropolis_repeats")
-def metro_rep_press_cb(page,pad):
-	for p in range(64):
-		col = p % 8
-		row = (p / 8) 
-		page.grid[p] = row <= page.outputarray[col]
-mr.addpresscallback(metro_rep_press_cb)
-
-def metro_rep_data_cb(page,pad):
-	if len(page.outputarray) ==0:
-		page.outputarray = [0] * 8
-	col = pad % 8
-	row = (pad / 8) 
-	page.outputarray[col] = row
-mr.adddatacallback( metro_rep_data_cb)
-
-##rpage 1
-dg = pm.addpage("metropolis_gate_dual")
-def metro_gate_press_cb(page,pad):
-	for p in range(64):
-		col = p % 8
-		row = (p / 8) % 4
-		sec =  int(p < 32) 
-
-		page.grid[p] = row <= page.outputarray[sec][col]
-
-dg.addpresscallback(metro_gate_press_cb)
-
-def metro_gate_data_cb(page,pad):
-	if len(page.outputarray) ==0:
-		page.outputarray = [[]] * 2
-		page.outputarray[0] = [0] * 8
-		page.outputarray[1] = [0] * 8
-	col = pad % 8
-	row = (pad / 8) % 4
-	sec =  int(pad < 32) 
-	page.outputarray[sec][col] = row
-dg.adddatacallback( metro_gate_data_cb)
-
-##rpage 2
-p = pm.addpage("metropolis_pitch")
-
-def metro_rep_press_cb(page,pad):
-	for p in range(64):
-		col = p % 8
-		row = (p / 8) 
-		page.grid[p] = row <= page.outputarray[col]
-p.addpresscallback(metro_rep_press_cb)
-
-def metro_rep_data_cb(page,pad):
-	if len(page.outputarray) ==0:
-		page.outputarray = [0] * 8
-	col = pad % 8
-	row = (pad / 8) 
-	page.outputarray[col] = row
-p.adddatacallback( metro_rep_data_cb)
 
 pm.setpage(0)
 
@@ -195,7 +198,6 @@ except (EOFError, KeyboardInterrupt):
 	print('')
 finally:
 	print("Exit.")
-	s.close()
 	midiin.close_port()
 	midiout.close_port()
 	del midiin
